fb7bcedb1831ac6c030441147558384714c1b1ea
Lars Müller <me@larsmueller.me>
2024-05-05 15:10:08 +0200

    Updated tabler Libs, Added NavBar

---
+++ SpotifyAuth/StringExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace SpotifyAuth;
+
+public static class StringExtension
+{
+    public static bool IsNullOrWhiteSpace(this string str)
+    {
+
+        if (str == null) return true;
+        if (str.Length == 0) return true;
+        if (str == string.Empty) return true;
+        if (str == "") return true;
+        return false;
+    }
+
+}
