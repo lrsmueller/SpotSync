2eef10d21f34426e11f420a0a30dfc4a521b495d
Lars Müller <me@larsmueller.me>
2024-05-05 15:08:55 +0200

    Added Spotify Claims Mapping + View

---
+++ SpotifyAuth/Components/Account/SpotifyClaimTypes.cs
@@ -0,0 +1,7 @@
+﻿namespace SpotifyAuth;
+
+public static class SpotifyClaimTypes
+{
+	public const string Product = "urn:spotify:product";
+	public const string ProfilePicture = "urn:spotify:profilepicture";
+}
--- SpotifyAuth/Program.cs
+++ SpotifyAuth/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using SpotifyAPI.Web;
 using SpotifyAuth;
 using SpotifyAuth.Components;
@@ -13,7 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+	.AddInteractiveServerComponents();
 
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<IdentityUserAccessor>();
@@ -25,70 +26,75 @@ builder.Services.AddScoped<SpotifyClientBuilder>();
 
 builder.Services.AddAuthorization(options =>
 {
-    options.AddPolicy("Spotify", policy =>
-    {
-        policy.AuthenticationSchemes.Add("Spotify");
-        policy.RequireAuthenticatedUser();
-    });
+	options.AddPolicy("Spotify", policy =>
+	{
+		policy.AuthenticationSchemes.Add("Spotify");
+		policy.RequireAuthenticatedUser();
+	});
 });
-
 builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultScheme = IdentityConstants.ApplicationScheme;
-        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-    })
-    .AddSpotify(options =>
-    {
-        options.ClientId = builder.Configuration["SPOTIFY_CLIENT_ID"] ?? throw new InvalidOperationException("SPOTIFY_CLIENT_ID not found.");
-        options.ClientSecret = builder.Configuration["SPOTIFY_CLIENT_SECRET"] ?? throw new InvalidOperationException("SPOTIFY_CLIENT_SECRET not found.");
-        options.CallbackPath = "/Auth/callback";
-        options.SaveTokens = true;
+	{
+		options.DefaultScheme = IdentityConstants.ApplicationScheme;
+		options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+	})
+	.AddSpotify(options =>
+	{
+		options.ClientId = builder.Configuration["SPOTIFY_CLIENT_ID"] ?? throw new InvalidOperationException("SPOTIFY_CLIENT_ID not found.");
+		options.ClientSecret = builder.Configuration["SPOTIFY_CLIENT_SECRET"] ?? throw new InvalidOperationException("SPOTIFY_CLIENT_SECRET not found.");
+		options.CallbackPath = "/Auth/callback";
+		options.SaveTokens = true;
 
-        var scopes = new List<string> {
-            UserReadEmail, UserReadPrivate, PlaylistReadPrivate, PlaylistReadCollaborative, PlaylistModifyPrivate, PlaylistModifyPublic, UserLibraryRead
-          };
-        options.Scope.Add(string.Join(",", scopes));
+		var scopes = new List<string> {
+			UserReadEmail, UserReadPrivate, PlaylistReadPrivate, PlaylistReadCollaborative, PlaylistModifyPrivate, PlaylistModifyPublic, UserLibraryRead
+		  };
+		options.Scope.Add(string.Join(",", scopes));
 
-        options.Events.OnCreatingTicket = ctx =>
-        {
-            var tokens = ctx.Properties.GetTokens();
-            ctx.Properties.StoreTokens(tokens);
-            
-            //TODO add save tokens to DB
-            return Task.CompletedTask;
-        };
-    })
-    .AddIdentityCookies();
+		//options.ClaimActions.MapAll();
+
+		options.ClaimActions.MapJsonKey(SpotifyClaimTypes.Product, "product", "string");
+		//options.ClaimActions.MapJsonKey("urn:spotify:product", "product", "string");
+		options.ClaimActions.MapJsonSubKey(SpotifyClaimTypes.ProfilePicture, "images", "url", "string");
+
+		options.Events.OnCreatingTicket = ctx =>
+		{
+			var tokens = ctx.Properties.GetTokens();
+			ctx.Properties.StoreTokens(tokens);
+
+			//TODO add save tokens to DB
+			return Task.CompletedTask;
+		};
+	})
+	.AddIdentityCookies();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+	options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options =>
-    {
-        options.SignIn.RequireConfirmedAccount = false;
-        options.SignIn.RequireConfirmedEmail = false;
-        options.SignIn.RequireConfirmedPhoneNumber = false;
-        options.User.RequireUniqueEmail = true;
-    })
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddSignInManager()
-    .AddDefaultTokenProviders();
+	{
+		options.SignIn.RequireConfirmedAccount = false;
+		options.SignIn.RequireConfirmedEmail = false;
+		options.SignIn.RequireConfirmedPhoneNumber = false;
+		options.User.RequireUniqueEmail = true;
+	})
+	.AddEntityFrameworkStores<ApplicationDbContext>()
+	.AddSignInManager()
+	.AddDefaultTokenProviders();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseMigrationsEndPoint();
-    app.UseDeveloperExceptionPage();
+	app.UseMigrationsEndPoint();
+	app.UseDeveloperExceptionPage();
 }
 else
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error", createScopeForErrors: true);
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -97,7 +103,7 @@ app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+	.AddInteractiveServerRenderMode();
 
 // Add additional endpoints required by the Identity /Account Razor components.
 app.MapAdditionalIdentityEndpoints();
