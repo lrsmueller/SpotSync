a3afa2522d995517b22b586fccaa24c4a4806df4
Lars Müller <me@larsmueller.me>
2024-05-12 19:33:07 +0200

    added Test Project

---
+++ src/SpotSync.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace SpotSync.Test;
+
+[TestClass]
+public class SpotifyRefreshBackgroundServiceTest
+{
+    
+    [TestMethod]
+    public void HappyTest()
+    {
+    }
+
+    [TestMethod]
+    public void EmptyTest() 
+    {
+    
+    }
+
+}
